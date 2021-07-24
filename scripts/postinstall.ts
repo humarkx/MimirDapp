7c6397850ffa17f842de2f6b8a4a545cb8c7be50
Hugo Marques <all@hugomarques.pt>
2021-07-24 15:59:33 +0100

    keep it running

--- scripts/postinstall.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-import * as child_process from 'child_process';
-import {macos} from 'platform-detect';
-
-if (macos) {
-  child_process.execSync('npx pod-install', { stdio: 'inherit' });
-  child_process.execSync('npm run build:ios', { stdio: 'inherit' });
-}
